281199eade928cbebf733df7433af64bdff92986
SebastianPortocarre <143566350+SebastianPortocarre@users.noreply.github.com>
2024-10-26 18:09:34 -0500

    Update Compressor.cpp

--- Compressor.cpp
+++ Compressor.cpp
@@ -5,21 +5,24 @@
 #include <string>
 using namespace std;
 
+// Función para comprimir los datos utilizando Zstandard
 vector<char> Compressor::compress(const string& data) {
     // Obtener el tamaño máximo del buffer comprimido
     size_t maxCompressedSize = ZSTD_compressBound(data.size());
     vector<char> compressedData(maxCompressedSize);
 
+    // Comprimir los datos utilizando Zstandard
     size_t compressedSize = ZSTD_compress(compressedData.data(), maxCompressedSize, data.data(), data.size(), 20);
     if (ZSTD_isError(compressedSize)) {
         cerr << "Error al comprimir los datos con Zstandard: " << ZSTD_getErrorName(compressedSize) << endl;
         return {};
     }
 
-    compressedData.resize(compressedSize); 
+    compressedData.resize(compressedSize);  // Ajustar el tamaño del vector comprimido
     return compressedData;
 }
 
+// Función para descomprimir los datos utilizando Zstandard
 string Compressor::decompress(const vector<char>& compressedData) {
     // Obtener el tamaño original de los datos a partir del flujo comprimido
     unsigned long long originalSize = ZSTD_getFrameContentSize(compressedData.data(), compressedData.size());
@@ -33,6 +36,7 @@ string Compressor::decompress(const vector<char>& compressedData) {
 
     vector<char> decompressedData(static_cast<size_t>(originalSize));
 
+    // Descomprimir los datos utilizando Zstandard
     size_t decompressedSize = ZSTD_decompress(decompressedData.data(), originalSize, compressedData.data(), compressedData.size());
     if (ZSTD_isError(decompressedSize)) {
         cerr << "Error al descomprimir los datos con Zstandard: " << ZSTD_getErrorName(decompressedSize) << endl;
